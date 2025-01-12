9e17aeca59356c8c0fb4bf6613fd7f770810360e
Gobbarakallu surendra <sgobbarakallu@gmail.com>
2025-01-12 21:11:57 +0530

    armstrong

---
+++ Armstrong.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    int n;
+    int res=0;
+    printf("enter the number");
+    scanf("%d",&n);
+    int m=n;
+    while(m!=0){
+       int rem=m%10;
+     res +=rem * rem * rem*rem;
+        m=m/10;
+    }
+    if(res==n)
+    printf("given number is Armstrong %d",n);
+    else
+    printf("given number is not Armstrong %d",n);
+
+}
