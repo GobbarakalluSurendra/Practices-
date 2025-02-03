0b449fa3fdb0529606eb48fe59abd84ff06b87f2
Gobbarakallu surendra <sgobbarakallu@gmail.com>
2025-02-03 17:47:03 +0530

    Add files via upload

---
+++ route.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const middleware=((req,res,next)=>{
+    if(10>20){
+        next()
+    }
+})
+
+app.get('/home',middleware, (req, res) => {
+    res.send("hello, I am the home page");
+});
+
+app.get('/about', (req, res) => {
+    res.send("hello, I am the about page");
+});
+
+app.get('/user/:id', (req, res) => { // Note the correction here
+    res.send(`you searched for ${req.params.id}`);
+});
+
+app.listen(port, () => {
+    console.log("Server started and running successfully");
+});
