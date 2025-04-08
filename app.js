5796c383cbc1a40f46c2a98dfe4006fbf8abeabb
Gobbarakallu surendra <sgobbarakallu@gmail.com>
2025-04-08 21:21:38 +0530

    app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const firstmiddleware=((req,res,next)=>{
         next()
     }
     else{
-        console.log("sorry you are not allowed")
+        console.log("you are not allowed")
     }
 })
 const secondmiddleware=((req,res,next)=>{
