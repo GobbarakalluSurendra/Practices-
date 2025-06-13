c908e11ae23b76726deddc7abb679055b069a4eb
Gobbarakallu surendra <sgobbarakallu@gmail.com>
2025-06-13 21:38:05 +0530

    CNOW.py

--- CNOW.py
+++ CNOW.py
@@ -2,4 +2,4 @@ import pandas as pd
 data = pd.read_csv("https://raw.githubusercontent.com/amankharwal/Website-data/master/articles.csv", encoding = 'latin1')
 data.head()
 data["Number of Words"] = data["Article"].apply(lambda n: len(n.split())) 
-data.head()
+Print(data.head()) 
