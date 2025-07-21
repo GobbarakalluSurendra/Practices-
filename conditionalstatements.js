fb6943d06f1a2078dc3c9a3ae51b06bd0c3bd987
Gobbarakallu surendra <sgobbarakallu@gmail.com>
2025-07-21 12:14:16 +0530

    Add files via upload

---
+++ conditionalstatements.js
@@ -0,0 +1,18 @@
+a=210, b=100, c=-23
+if(a>b)
+    console.log("a is bigger")
+else if(a>c)
+    console.log(" a  is bigger")
+else
+    console.log("a is smaller")
+
+
+    switch(a)
+    {
+        case 10:
+            console.log("value is 10")
+           break
+        case 20:console.log("value is 20") 
+            break 
+        default : console.log("entry valid choice")      
+    }
